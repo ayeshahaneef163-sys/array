8942e07f56656b0d5a7dd922b2a67eb1605e0c22
Ayesha Haneef <ayeshahaneef163@gmail.com>
2026-02-21 15:40:00 +0500

    Print Array

--- printArray.cpp
+++ printArray.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main(){
-    cout << "Print Array." << endl;
+    int arr[5] = {1, 2, 3, 4, 5};
+    int n = 5;
+    for(int i = 0; i <= n; i++){
+        cout << "Array is: " << arr[i] << endl;
+    }   
     return 0;
 }
